9bba5d3c7f9b86fcbf1aba7898347df779194cf4
Horácio Dias <horacio.dias92@gmail.com>
2016-08-30 19:58:54 -0300

    Atualizando o servidor do tornado

--- testes/tornado-run.py
+++ testes/tornado-run.py
@@ -3,20 +3,36 @@ import sys, getopt
 import tornado.ioloop
 import tornado.web
 import json
-
+import hashlib
+reload(sys)
+sys.setdefaultencoding("latin-1")
 
 class MainHandler(tornado.web.RequestHandler):
 
     lista = []
 
     def post(self):
-        data = json.loads(self.request.body)
-        MainHandler.lista.append(data)
-        self.write('{"success":1,"message":"OK"}')
+        try:
+            print(self.request.body.encode('utf-8') )
+            data = json.loads(self.request.body.encode('utf-8'))
+            if data.get('nome') and data.get('idade'):
+                MainHandler.lista.append(data)
+                md5 = hashlib.md5()
+                md5.update( data['nome'] )
+                print(md5.hexdigest())
+                self.write('{"success":1,"message":"OK"}')
+            else:
+                self.write('{"success":1,"message":"Objeto não contém os atributos nome e idade "}')
+        except Exception as ex:
+            self.write('{"success":1,"message":"JSON não pode ser parseado %s"}'%(ex))
 
     def get(self):
         if MainHandler.lista:
             self.write(json.dumps(MainHandler.lista))
+            for data in MainHandler.lista:
+                md5 = hashlib.md5()
+                md5.update( data['nome'] )
+                print(md5.hexdigest())
         else:
             self.write('[]')
 
